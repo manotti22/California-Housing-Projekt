7be6cbf6a570cb7815324069fb4c22ec2c70ae9d
manotti22 <ottizoubairi@gmail.com>
2022-07-25 18:33:13 +0200

    updated

--- package/util/util.py
+++ package/util/util.py
@@ -7,6 +7,19 @@ import pandas as pd
 from package.bestandteil import *
 
 
+def write_yaml_file(file_path:str,data:dict=None):
+    """
+    Create yaml file 
+    file_path: str
+    data: dict
+    """
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path,"w") as yaml_file:
+            if data is not None:
+                yaml.dump(data,yaml_file)
+    except Exception as e:
+        raise PackageException(e,sys)
 
 
 
@@ -16,7 +29,7 @@ def read_yaml_file(file_path:str)->dict:
     file_path: str
     """
     try:
-        with open(file_path, 'rb') as yaml_file:
+        with open(file_path,'rb') as yaml_file:
             return yaml.safe_load(yaml_file)
     except Exception as e:
         raise PackageException(e,sys) from e
