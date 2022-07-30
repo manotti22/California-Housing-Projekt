6b70cf2809e309857550fbb54da9215f197e563d
manotti22 <ottizoubairi@gmail.com>
2022-07-30 19:14:38 +0200

    completed

--- app.py
+++ app.py
@@ -24,15 +24,13 @@ MODEL_SCHMUCK_FILE_PATH = os.path.join(ROOT_DIR, SCHMUCK_DIR, "model.yaml")
 LOG_DIR = os.path.join(ROOT_DIR, LOG_FOLDER_NAME)
 PIPELINE_DIR = os.path.join(ROOT_DIR, PIPELINE_FOLDER_NAME)
 MODEL_DIR = os.path.join(ROOT_DIR, SAVED_MODELS_DIR_NAME)
-
-
 from package.logger import get_log_dataframe
+
 
 PACKAGE_DATA_KEY = "package_data"
 PRODTAKEN_KEY = "ProdTaken"
 
 app = Flask(__name__)
-
 
 @app.route('/ordner', defaults={'class_path': 'Package'})
 @app.route('/ordner/<path:class_path>')
@@ -111,49 +109,68 @@ def predict():
     }
 
     if request.method == 'POST':
-        longitude = float(request.form['longitude'])
-        latitude = float(request.form['latitude'])
-        housing_median_age = float(request.form['housing_median_age'])
-        total_rooms = float(request.form['total_rooms'])
-        total_bedrooms = float(request.form['total_bedrooms'])
-        population = float(request.form['population'])
-        households = float(request.form['households'])
-        median_income = float(request.form['median_income'])
-        ocean_proximity = request.form['ocean_proximity']
+          Age = float(request.form['Age'])
+          TypeofContact =request.form['TypeofContact']
+          CityTier        = int(request.form['CityTier'])
+          DurationOfPitch = float(request.form['DurationOfPitch '])
+          Occupation      =  request.form['Occupation']  
+          Gender          =  request.form['Gender']   
+          NumberOfPersonVisiting = int(request.form['NumberOfPersonVisiting'])
+          NumberOfFollowups = float(request.form['NumberOfFollowups'])
+          PreferredPropertyStar =float(request.form['PreferredPropertyStar'])
+          MaritalStatus  =   request.form['MaritalStatus']
+          NumberOfTrips  =  float(request.form['umberOfTrips'])   
+          Passport       =   int(request.form['Passport'])   
+          ProductPitched  =   request.form['ProductPitched']      
+          PitchSatisfactionScore  = int(request.form['PitchSatisfactionScore'])   
+          OwnCar                  = int(request.form['OwnCar'])   
+          NumberOfChildrenVisiting =float(request.form['umberOfChildrenVisiting'])   
+          Designation   = request.form['Designation']
+          MonthlyIncome  = float(request.form['MonthlyIncome'])   
 
-        housing_data = HousingData(longitude=longitude,
-                                   latitude=latitude,
-                                   housing_median_age=housing_median_age,
-                                   total_rooms=total_rooms,
-                                   total_bedrooms=total_bedrooms,
-                                   population=population,
-                                   households=households,
-                                   median_income=median_income,
-                                   ocean_proximity=ocean_proximity,
+
+          package_data = PackageData(Age = Age,
+                                   TypofContact =TypeofContact,  
+                                    CityTier =CityTier,
+                                    DurationOfPitch=DurationOfPitch,
+                                     Occupation=Occupation,
+                                    Gender =Gender,
+                                    NumberOfPersonVisiting=NumberOfPersonVisiting,
+                                    NumberOfFollowups=NumberOfFollowups,
+                                    PreferredPropertyStar=PreferredPropertyStar,
+                                    MaritalStatus = MaritalStatus,
+                                     NumberOfTrips =NumberOfTrips,
+                                    Passport =Passport,
+                                     ProductPitched = ProductPitched,
+                                      PitchSatisfactionScore = PitchSatisfactionScore,
+                                     OwnCar = OwnCar,  
+                                    NumberOfChildrenVisiting = NumberOfChildrenVisiting,
+                                    Designation =  Designation, 
+                                    MonthlyIncome = MonthlyIncome 
                                    )
-        housing_df = housing_data.get_housing_input_data_frame()
-        housing_predictor = HousingPredictor(model_dir=MODEL_DIR)
-        median_housing_value = housing_predictor.predict(X=housing_df)
-        context = {
-            HOUSING_DATA_KEY: housing_data.get_housing_data_as_dict(),
-            MEDIAN_HOUSING_VALUE_KEY: median_housing_value,
-        }
-        return render_template('predict.html', context=context)
+          package_df = package_data.get_package_input_data_frame()
+          package_predictor = PackagePredictor(model_dir=MODEL_DIR)
+          ProdTaken= package_predictor.predict(X=package_df)
+          context = {
+            PACKAGE_DATA_KEY: package_data.get_package_data_as_dict(),
+            PRODTAKEN_KEY: ProdTaken,
+             }
+          return render_template('predict.html', context=context)
     return render_template("predict.html", context=context)
 
-
-@app.route('/saved_models', defaults={'req_path': 'saved_models'})
-@app.route('/saved_models/<path:req_path>')
-def saved_models_dir(req_path):
+@app.route('/saved_models', defaults={'class_path': 'saved_models'})
+@app.route('/saved_models/<path:class_path>')
+def saved_models_dir(class_path):
     os.makedirs("saved_models", exist_ok=True)
     # Joining the base and the requested path
-    print(f"req_path: {req_path}")
-    abs_path = os.path.join(req_path)
+    print(f"class_path: {class_path}")
+    abs_path = os.path.join(class_path)
     print(abs_path)
     # Return 404 if path doesn't exist
     if not os.path.exists(abs_path):
         return abort(404)
 
+    
     # Check if path is a file and serve
     if os.path.isfile(abs_path):
         return send_file(abs_path)
@@ -169,32 +186,32 @@ def saved_models_dir(req_path):
     return render_template('saved_models_files.html', result=result)
 
 
-@app.route("/update_model_config", methods=['GET', 'POST'])
-def update_model_config():
+@app.route("/update_model_schmuck", methods=['GET', 'POST'])
+def update_model_schmuck():
     try:
         if request.method == 'POST':
-            model_config = request.form['new_model_config']
-            model_config = model_config.replace("'", '"')
-            print(model_config)
-            model_config = json.loads(model_config)
+            model_schmuck = request.form['new_model_schmuck']
+            model_schmuck = model_schmuck.replace("'", '"')
+            print(model_schmuck)
+            model_schmuck = json.loads(model_schmuck)
 
-            write_yaml_file(file_path=MODEL_CONFIG_FILE_PATH, data=model_config)
+            write_yaml_file(file_path=MODEL_SCHMUCK_FILE_PATH, data=model_schmuck)
 
-        model_config = read_yaml_file(file_path=MODEL_CONFIG_FILE_PATH)
-        return render_template('update_model.html', result={"model_config": model_config})
+        model_schmuck = read_yaml_file(file_path=MODEL_SCHMUCK_FILE_PATH)
+        return render_template('update_model.html', result={"model_schmuck": model_schmuck})
 
     except  Exception as e:
         logging.exception(e)
         return str(e)
 
 
-@app.route(f'/logs', defaults={'req_path': f'{LOG_FOLDER_NAME}'})
-@app.route(f'/{LOG_FOLDER_NAME}/<path:req_path>')
-def render_log_dir(req_path):
+@app.route(f'/logs', defaults={'class_path': f'{LOG_FOLDER_NAME}'})
+@app.route(f'/{LOG_FOLDER_NAME}/<path:class_path>')
+def render_log_dir(class_path):
     os.makedirs(LOG_FOLDER_NAME, exist_ok=True)
     # Joining the base and the requested path
-    logging.info(f"req_path: {req_path}")
-    abs_path = os.path.join(req_path)
+    logging.info(f"req_path: {class_path}")
+    abs_path = os.path.join(class_path)
     print(abs_path)
     # Return 404 if path doesn't exist
     if not os.path.exists(abs_path):
--- demo.py
+++ demo.py
@@ -3,12 +3,17 @@ from package.exception import PackageException
 from package.logger import logging
 from package.structure.structure import structure
 from package.haupteil.data_transform import DataTransformation
+import os
 
 def main():
     try:
+        schmuck_path = os.path.join("schmuck","schmuck.yaml")
+        pipeline = Pipeline(structure(schmuck_file_path=schmuck_path))
         print("Hello")
         pipeline = Pipeline()
-        pipeline.run_pipeline()
+        #pipeline.run_pipeline()
+        pipeline.start()
+        logging.info("main function execution completed.")
         #data_validation_ordner = structure().get_data_transformation_schmuck()
         #print(data_validation_ordner)
         
